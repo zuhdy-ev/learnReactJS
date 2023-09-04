132ef7db66d33073a2756906658507c34351c87c
zuhdy-ev <ardhiekov@gmail.com>
2023-09-04 18:29:57 +0700

    create search bar with search-button (no real-time searching method)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,15 +3,25 @@ import { useState } from "react";
 function Search(props) {
   const [search, setSearch] = useState("");
 
-  const onSearchChange = (event) => {
-    setSearch(event.target.value);
-    props.onSearchChange(event.target.value);
+  const onSearchChange = () => {
+    props.onSearchChange(search);
+  };
+
+  const searchKeydown = (e) => {
+    if (e.key === "Enter") {
+      onSearchChange();
+    }
   };
 
   return (
     <>
       <div>
-        Cari Artikel : <input onChange={onSearchChange}></input>
+        Cari Artikel :{" "}
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          onClick={searchKeydown}
+        ></input>
+        <button onClick={searchKeydown}>Cari</button>
       </div>
       <small>
         Ditemukan {props.totalPosts} data dengan kata kunci: {search}
